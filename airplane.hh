446edc346997618dd47c0f4f1ad244314ff5fa2f
jparisi01 <jparisi@stevens.edu>
2021-10-08 14:19:30 -0400

    changed xyz variable types to double

--- airplane.hh
+++ airplane.hh
@@ -6,7 +6,7 @@
 //Should inherit from robot
 class airplane : public robot {
   public:
-  airplane(std::string name, int32_t x, int32_t y, int32_t z) : name(name), x(x), y(y), z(z) {}
+  airplane(const string& name, double x, double y, double z) : name(name), x(x), y(y), z(z) {}
 
   friend std::ostream& operator <<(ostream& s, const airplane& a);
 };

9028fd2e61c683495992a4620d1418b327db24b4
Jake Foglia <jakefoglia@outlook.com>
2021-10-08 14:29:43 -0400

    added default params for airplane constructor

--- airplane.cc
+++ airplane.cc
@@ -1,6 +1,6 @@
 #include "airplane.hh"
 
-airplane::airplane(const std::string& name, double x, double y, double z) : name(name), x(x), y(y), z(z) { }
+airplane::airplane(const std::string& name, double x = 0, double y = 0, double z = 0) : name(name), x(x), y(y), z(z) { }
 
 std::ostream& operator <<(std::ostream& s, const airplane& a)
 {

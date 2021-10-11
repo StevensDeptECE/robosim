57ed326d09749f216b4ea6b6c41228f210d82b18
Pavlos Tziranis <ptzirani@stevens.edu>
2021-10-11 18:26:35 -0400

    Created cc skeleton of beacon, Still adding error

--- beacon.cc
+++ beacon.cc
@@ -1,5 +1,21 @@
 #include "beacon.hh"
-#include <iostream> 
-using namespace std;
 
-beacon::beacon(const std::string& name, const vec_3d& location): name(name), location(location){};
+#include <iostream>
+#include <string>
+
+#include "vec_3d.hh"
+
+// Authors: Anastasios Zochios, Pavlos Tziranis
+
+beacon::beacon(std::string name, vec_3d location, double localError = 0.05,
+               double rangeError)
+    : name(name),
+      location(location),
+      localError(localError),
+      rangeError(rangeError) {}
+
+// beacon::double detect_object(double power);
+/* Adding a scalar to a vector; vec_3d operator+ needs to initialize a scalar as
+ * vector */
+friend ostream& operator<<(ostream& s, beacon a) {
+  return s << a.name << a.location + a.localError;

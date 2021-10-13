592cc27b7b7dc73efc1cd3e67275e542bd2c79b4
Jake Foglia <jakefoglia@outlook.com>
2021-10-13 12:25:24 -0400

    updated airplane files to support pitch, yaw, airspeed, and lookAt - which sets pitch and yaw, as well as an update function and a test program. pair programmed with James Parisi

--- airplane.hh
+++ airplane.hh
@@ -1,12 +1,28 @@
 #pragma once
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <string>
-#include "robot.hh"
+#include <cmath>
+//#include "robot.hh"
 
-//Should inherit from robot
-class airplane : public robot {
-  public:
-  airplane(const string& name, double x, double y, double z) : name(name), x(x), y(y), z(z) {}
+//should eventually inherit from robot
+class airplane{// : public robot {
+public:
+  airplane(const std::string& name, double x = 0, double y = 0, double z = 0);
+  void lookAt(double dx, double dy, double dz);
+  void setYaw(double y);
+  void setPitch(double p);
+  void setAirSpeed(double s);
+  void update(double dt);
 
-  friend std::ostream& operator <<(ostream& s, const airplane& a);
+  friend std::ostream& operator <<(std::ostream& s, const airplane& a);
+
+private:
+  std::string name; 
+  double x, y, z;
+
+  double airSpeed; // [0, inf]
+  double yaw;   // [-Pi, Pi] relative to +y axis on the x,y plane
+  double pitch; // [-Pi, Pi] horizon relative. in other words, pitch along z relative to x,y plane
 };
+

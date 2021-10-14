76396429dcf0445b5db2b2334de4d0c0c8cdaf4a
jvillani <jvillan1@stevens.edu>
2021-10-14 00:00:57 -0400

    Added << and >> operators

--- submarine.cc
+++ submarine.cc
@@ -5,10 +5,25 @@
 #incldue <string>
 #include "submarine.hh"
 
-class submarine : submarine(const std:: string& name, double x,double y, double velX,
-			    double velY,):name(name),x(x), y(y),velX(velX),velY(velY){
+submarine::submarine(const std:: string& name, double x,double y, double velX,
+			    double velY):name(name),x(x), y(y),velX(velX),velY(velY){  
+}
 
-  
+std::ostream& operator <<(std::ostream& s, const submarine b&){
+  return s << "Name: " << b.name << '\n';
+  return s << "Velocity: (" << b.velX << ", "  << b.velY << ")" << '\n';
+  return s << "Location: (" << b.x << ", " << b.y << ")" << '\n';
+}
+
+std::istream& operator >>(std::istream& i, submarine b){
+  cout << "Enter X Location" << '\n';
+  i >> b.x
+  cout << "Enter Y Location" << '\n';
+  i >> b.y
+  cout << "Enter X Velocity" << '\n';
+  i >> b.velX
+  cout << "Enter Y Velocity" << '\n';
+  i >> b.velY
 }
 
 

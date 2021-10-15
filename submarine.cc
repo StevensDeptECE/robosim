c3b1d0f11f9a5a7de123446d13d26beb7ad82b11
jvillani <jvillan1@stevens.edu>
2021-10-15 00:14:32 -0400

    Changed types to vec_3d and set basis for movement funcs

--- submarine.cc
+++ submarine.cc
@@ -5,25 +5,27 @@
 #incldue <string>
 #include "submarine.hh"
 
-submarine::submarine(const std:: string& name, double x,double y, double velX,
-			    double velY):name(name),x(x), y(y),velX(velX),velY(velY){  
-}
+submarine::submarine(const std:: string& name,vec_3d pos,vec_3d vel,uint16_t max_speed,
+		     int32_t max_depth, bool beached, int32_t rudder_ang, int32_t prop_spd,
+		     int32_t plane_ang):name(name)),pos(pos),vel(vel),max_speed(max_speed),
+					max_depth(max_depth),beached(beached),rudder_ang(rudder_ang),
+				        prop_spd(prop_spd),plane_ang(plane_ang){}
 
 std::ostream& operator <<(std::ostream& s, const submarine b&){
   return s << "Name: " << b.name << '\n';
-  return s << "Velocity: (" << b.velX << ", "  << b.velY << ")" << '\n';
-  return s << "Location: (" << b.x << ", " << b.y << ")" << '\n';
+  return s << "Velocity: (" << b.vel << '\n';
+  return s << "Location: (" << b.pos << '\n';
 }
 
 std::istream& operator >>(std::istream& i, submarine b){
-  cout << "Enter X Location" << '\n';
-  i >> b.x
-  cout << "Enter Y Location" << '\n';
-  i >> b.y
-  cout << "Enter X Velocity" << '\n';
-  i >> b.velX
-  cout << "Enter Y Velocity" << '\n';
-  i >> b.velY
+
 }
 
-
+void setRudder(){//Change X/Y Direction
+}
+void setProp(){ //Change X/Y Velocity
+}
+void setPlane(){ //Move in Z
+}
+void move(){//Combination
+}

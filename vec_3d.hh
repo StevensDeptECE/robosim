022c5898d57e477a2404e77e36598e1689bd93a7
gauthampkrishnan@ieee.org <gauthampkrishnan@ieee.org>
2021-10-14 23:19:54 -0400

    Vec3d commit

--- vec_3d.hh
+++ vec_3d.hh
@@ -1,7 +1,32 @@
-//Gautham Prem Krishnan
+/*
+ Gautham Prem Krishnan
+*/
 
 #pragma once
-#include<iostream>
-class vec_3d{
+#include <iostream>
+#include "robot.hh";
+#include "beacon";
+using namespace std;
 
-};
+ class vec_3d{
+  private:
+   double x1,y1,x2,y2;
+
+  public:
+   vec_3d() : x1(0), y1(0),x2(0),y2(0){}
+   vec_3d(double x_input, double y_input, double z_input):x1(x_input),y1(y_input),z1(z_input){}
+   vec_3d(double x_input, double y_input) :x1(x_input),y1(y_input),z1(0){}
+   vec_3d(double x_input) : x1(x_input), y1(0), z1(0){
+   }
+
+
+   double batterylife() const{
+     cout<<"Battery Life";
+   }
+
+   double location(const vec_3d& a,const vec_3d& b){
+   double ans = (a.x1 - a.x1, b.y1-b.y2);
+   return ans;
+ }
+ };
+

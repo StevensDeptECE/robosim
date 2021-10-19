6bb77d27d61ef8cc08ae812963ef8420bb45524c
chuanhanyuan <cyuan7@stevens.edu>
2021-10-18 22:58:52 -0400

    change cc file

--- vec_3d.cc
+++ vec_3d.cc
@@ -1,37 +1,82 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
+#include "vec_3d.hh"
 
-class vec_3d {
- private:
-  double vec1, vec2, vec3;
+vec_3d::vec_3d(int x, int y, int z) : x(p_x), y(p_y), z(p_z) {}
 
- public:
-  vec_3d(double x = 0.0, double y = 0.0, double z = 0.0)
-      : vec1(x), vec2(y), vec3(z) {}
-  friend vec_3d operator+(vec_3d a, vec_3d b) {
-    return vec_3d(a.vec1 + b.vec1, a.vec2 + b.vec2, a.vec3 + b.vec3);
-  }
-  friend vec_3d operator-(vec_3d a, vec_3d b) {
-    return vec_3d(a.vec1 - b.vec1, a.vec2 - b.vec2, a.vec3 - b.vec3);
-  }
-  friend vec_3d operator-(vec_3d a) {
-    return vec_3d(-a.vec1, -a.vec2, -a.vec3);
-  }
-  friend ostream& operator<<(ostream& s, vec_3d c) {
-    return s << "(" << c.vec1 << ',' << c.vec2 << ',' << c.vec3 << ")";
-  }
-  friend vec_3d dot(vec_3d a, vec_3d b) {
-    return d = (a.vec1 * b.vec1) + (a.vec2 * b.vec2) + (a.vec3 * b.vec3);
-  }
-  friend istream& operator>>(istream& s, vec_3d c) {
-    return s >> c.vec1 >> c.vec2 >> c.vec3;
-  }
-  friend vec_3d mag(vec_3d a) {
-    double ans = (a.vec1 * a.vec1) + (a.vec2 * a.vec2) + (a.vec3 * a.vec3);
-    return sqrt(ans);
-  }
-  friend vec_3d magsq(vec_3d a) {
-    return (a.vec1 * a.vec1) + (a.vec2 * a.vec2) + (a.vec3 * a.vec3);
-  }
-};
+friend vec_3d operator+(vec_3d add_num) {
+  vec_3d result;
+  result.x = x + add_num.x;
+  result.y = y + add_num.y;
+  result.z = z + add_num.z;
+  return result;
+}
+friend vec_3d operator-(vec_3d minus_num) {
+  vec_3d result;
+  result.x = x - minus_num.x;
+  result.y = y - minus_num.y;
+  result.z = z - minus_num.z;
+
+  return result;
+}
+friend vec_3d operator-() {
+  vec_3d result;
+  result.x = -x;
+  result.y = -y;
+  result.z = -z;
+  return result;
+}
+friend double mag() {
+  double mag_result;
+  double a = abs(x);
+  double b = abs(y);
+  double c = abs(z);
+  mag_result = a + b + c;
+
+  return mag_result;
+}
+friend double magsq() {
+  double magsq_result;
+  double a = x;
+  double b = y;
+  double c = z;
+  magsq_result = a * a + b * b + c * c;
+
+  return magsq_result;
+}
+friend double dot(vec_3d num) {
+  double dot_result;
+  dot_result = x * num.x + y * num.y + z * num.z;
+
+  return dot_result;
+}
+
+friend double manhattan_distance(vec_3d num) {
+  double md_result;
+  double a = abs(x - num.x);
+  double b = abs(y - num.y);
+  double c = abs(z - num.z);
+
+  md_result = a + b + c;
+  return md_result;
+}
+friend double dot(vec_3d num_1, vec_3d num_2) {
+  double result;
+  result = num_1.x * num_2.x + num_1.y * num_2.y + num_1.z * num_2.z;
+  return result;
+}
+
+// Overload the << operator
+friend ostream& operator<<(ostream& p_cout, vec_3d& num) {
+  p_cout << "(" << num.x << "," << num.y << "," << num.z << ")";
+  return p_cout;
+}
+
+// Overload the >> operator
+friend istream& operator>>(istream& p_cin, vec_3d& num) {
+  p_cin >> num.x >> num.y >> num.z;
+  return p_cin;
+}
+}
+;

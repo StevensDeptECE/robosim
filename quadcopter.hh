0c6bbab56f309b528958665d924ab79d7b039ee1
ahuston-0 <aliceghuston@gmail.com>
2021-10-27 14:30:45 -0400

    clear up some namespace issues and get the present files compiling

--- quadcopter.hh
+++ quadcopter.hh
@@ -1,29 +1,27 @@
-//Irakli Okruashvili
+// Irakli Okruashvili
 #pragma once
 #include <iostream>
 #include <string>
+
 #include "robot.hh"
 
-using namespace std;
-
 class quadcopter : public robot {
-    public:
-        quadcopter(const string& name, double x, double y, double z) : name(name), x(x), y(y), z(z) {}
-class quadcopter{
-    public:
-        quadcopter(const string& name, double x = 0, double y = 0, double z = 0) {}
-        void loc_change(double dx, double dy, double dz);
-        void setThrottle(double h);
-        void setRaw(double r);
-        void setPitch(double p);
-        void setYaw(double w);
-        void setAirSpeed(double s);
-        void update(double dt);
+ public:
+  quadcopter(const string& name, double x, double y, double z)
+      : name(name), x(x), y(y), z(z) {}
+  class quadcopter {
+   public:
+    quadcopter(const string& name, double x = 0, double y = 0, double z = 0) {}
+    void loc_change(double dx, double dy, double dz);
+    void setThrottle(double h);
+    void setRaw(double r);
+    void setPitch(double p);
+    void setYaw(double w);
+    void setAirSpeed(double s);
+    void update(double dt);
 
-        friend ostream& operator <<(ostream& s, const quadcopter& a);
-}
-    private:
-        string name;
-        double x, y, z;
-        double throttle, raw, pitch, yaw, airSpeed;
+    friend ostream& operator<<(ostream& s, const quadcopter& a);
+  } private : std::string name;
+  double x, y, z;
+  double throttle, raw, pitch, yaw, airSpeed;
 };
--- robot.cc
+++ robot.cc
@@ -5,9 +5,12 @@ date : Oct/11
 */
 #include "robot.hh"
 
-robot::robot(double x, double y, double z, double BatteryLife) : location(x, y, z), BatteryLife(BatteryLife){}
+using namespace std;
 
-std::ostream& operator<< (std::ostream& a, robot & xyz) {
-        a << xyz.location << "batterylife=" << xyz.BatteryLife << endl;
-        return a; 
-    }
+robot::robot(double x, double y, double z, double BatteryLife)
+    : location(x, y, z), BatteryLife(BatteryLife) {}
+
+std::ostream& operator<<(std::ostream& a, robot& xyz) {
+  a << xyz.location << "batterylife=" << xyz.BatteryLife << endl;
+  return a;
+}

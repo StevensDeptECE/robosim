1937fd873e1c5fe70cfd2a66ba277a8e0b756503
Tasos Zochios <azochios@stevens.edu>
2021-12-16 14:02:49 -0500

    Proper use of robot methods in quadcopter

--- quadcopter.cc
+++ quadcopter.cc
@@ -1,33 +1,30 @@
 #include "quadcopter.hh"
 
+// Collaborators: Pavlos Tziranis, Aldrin Padua, Anastasios Zochios
 
 
-class quadcopter : public robot {
-    public:
-        quadcopter(double x, 
+quadcopter::quadcopter(std::string name, double x, 
             double y, double z, double hvar, double vvar, double hdg, double speed, double battlife) 
-            : robot(vec_3d(x, y, z), hvar, vvar, hdg, speed, battlife) {};
+            : robot(name, x, y, z, hvar, vvar, hdg, speed, battlife) {};
 
-        void loc_change(double dx, double dy, double dz);
 
-        void setThrottle(double h);
+ostream& operator <<(ostream& s, const quadcopter& a) {
+    return s << a.getName() << a.getLocation();
+}
+//TODO
+void loc_change(double dx, double dy, double dz) {}
 
-        void setRaw(double r);
+void setThrottle(double h) {}
 
-        void setPitch(double p);
+void setRaw(double r) {}
 
-        void setYaw(double w);
+void setPitch(double p) {}
 
-        void setAirSpeed(double s);
+void setYaw(double w) {}
+
+void setAirSpeed(double s) {}
         
-        void update(double dt);
+void update(double dt){}
+//
 
-        friend ostream& operator <<(ostream& s, const quadcopter& a);
 
-    private:
-        double throttle;
-        double raw;
-        double pitch;
-        double yaw; 
-        double airSpeed;
-};
--- quadcopter.hh
+++ quadcopter.hh
@@ -8,9 +8,11 @@
 // Collaborators: Pavlos Tziranis, Aldrin Padua, Anastasios Zochios
 
 class quadcopter : public robot {
-<<<<<<< HEAD
+
     public:
-        quadcopter(double x, double y, double z, double hvar, double vvar, double hdg, double speed, double battlife);
+        quadcopter(std::string name, double x, double y, double z, double hvar, double vvar, double hdg, double speed, double battlife);
+
+        void tick();
 
         void loc_change(double dx, double dy, double dz);
 
@@ -29,30 +31,10 @@ class quadcopter : public robot {
         friend ostream& operator <<(ostream& s, const quadcopter& a);
 
     private:
+        std::string name;
         double throttle;
         double raw;
         double pitch;
         double yaw; 
         double airSpeed;
-=======
-private:
-  string name;
-  vec_3d location;
-  double x, y, z;
-  double throttle, raw, pitch, yaw, airSpeed;
-
-public:
-  quadcopter(const string& name="", const vec_3d& pos=vec_3d(0,0,0)) : name(name), location(pos) {}
-  quadcopter(const string& name="", double x = 0, double y = 0, double z = 0) : name(name), location(vec_3d(x, y, z)) {}
-  void tick();
-  void loc_change(double dx, double dy, double dz);
-  void setThrottle(double h);
-  void setRaw(double r);
-  void setPitch(double p);
-  void setYaw(double w);
-  void setAirSpeed(double s);
-  void update(double dt);
-
-  friend ostream& operator <<(ostream& s, const quadcopter& a);
->>>>>>> c8763c539bd05d822033960176b1d32ff0116067
 };

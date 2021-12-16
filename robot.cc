0eb1139190ecd7cad4fd9d296cb8a73f145a1572
aleks-dimoski <aleksdimoski890@gmail.com>
2021-12-16 11:56:34 -0500

    Added est Location function for robot

--- robot.cc
+++ robot.cc
@@ -1,14 +1,51 @@
 /*
-Author:Wang,YenHsiang
-version : 1.0
-date : Oct/11
+Author:Wang,YenHsiang and Aleksandar Dimoski
+version : 1.1
+date : Dec 16th, 2021
 */
 #include "robot.hh"
 
-robot::robot(const vec_3d& pos, double BatteryLife) : location(pos), BatteryLife(BatteryLife) {}
-robot::robot(double x, double y, double z, double BatteryLife) : location(x, y, z), BatteryLife(BatteryLife){}
+#include <vector>
 
-std::ostream& operator<< (std::ostream& a, robot & xyz) {
-        a << xyz.location << "batterylife=" << xyz.BatteryLife << endl;
-        return a; 
-    }
+#include "beacon.hh"
+
+robot::robot(string name, double x, double y, double z, double posVarHoriz,
+             double posVarVert, double heading, double speed,
+             double headingVert, double BatteryLife = 100)
+    : name(name),
+      location(vec_3d(x, y, z)),
+      posVarHoriz(posVarHoriz),
+      posVarVert(posVarVert),
+      heading(heading),
+      speed(speed),
+      headingVert(headingVert),
+      BatteryLife(BatteryLife) {}
+
+robot::robot(string name, const vec_3d& pos, double posVarHoriz,
+             double posVarVert, double heading, double speed,
+             double headingVert, double BatteryLife = 100)
+    : name(name),
+      location(pos),
+      posVarHoriz(posVarHoriz),
+      posVarVert(posVarVert),
+      heading(heading),
+      speed(speed),
+      headingVert(headingVert),
+      BatteryLife(BatteryLife) {}
+
+vec_3d robot::getEstLocation(vector<beacon> beacons) {
+  vector<double> dists;
+  for (beacon b : beacons) {
+    dists.push_back(location.dist(b));
+  }
+  // TODO Calculate estimated position and return it; for now just returns
+  // actual position
+  return location;
+}
+
+std::ostream& robot::operator<<(std::ostream& s, robot& r) {
+  s << "The object " << r.getName() << " is "
+    << "Location= " << r.getLocation() << "\tBattery Life= " << r.getBatLife()
+    << endl;
+  return s;
+}
--- robot.hh
+++ robot.hh
@@ -1,28 +1,31 @@
 /*
-    Author : Ming-Hung Yen
+    Author : Ming-Hung Yen and Aleksandar Dimoski
 */
 
-#include "vec_3d.hh" 
 #include "beacon.hh"
+#include "vec_3d.hh"
 
 extern beacon TestBeacon;
 
 class robot {
-private:
-    vec_3d location;
-    double BatteryLife;
-    
-public:
- robot(const vec_3d& location, double BatteryLife);
- robot(double x, double y, double z, double BatteryLife);
+ private:
+  string name;
+  vec_3d location;
+  double BatteryLife, posVarHoriz, posVarVert, heading, speed, headingVert;
 
- // void tick();
- vec_3d estlocation = location.dist(TestBeacon);
+ public:
+  //  robot(const vec_3d& location, double BatteryLife);
+  //  robot(double x, double y, double z, double BatteryLife);
+  robot(string name, double x, double y, double z, double posVarHoriz,
+        double posVarVert, double heading, double speed, double headingVert);
+  robot(string name, const vec_3d& pos, double posVarHoriz, double posVarVert,
+        double heading, double speed, double headingVert);
 
- friend ostream& operator<<(ostream& a, robot& xyz) {
-   a << xyz.location << "batterylife=" << xyz.BatteryLife << endl;
-   return a; 
-    }
+  // void tick();
+  vec_3d estlocation = location.dist(TestBeacon);
 
-
+  friend ostream& operator<<(ostream& a, robot& xyz) {
+    a << xyz.location << "batterylife=" << xyz.BatteryLife << endl;
+    return a;
+  }
 };

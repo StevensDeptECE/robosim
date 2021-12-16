bbc56fc14c9fbd429775e31e29591b40648f2128
drino02 <apadua@stevens.edu>
2021-12-16 11:32:22 -0500

    Finished improving the beacon.hh and beacon.cc files

--- beacon.hh
+++ beacon.hh
@@ -3,19 +3,22 @@
 #include <iostream>
 #include <string>
 
+#include "random.hh"
 #include "vec_3d.hh"
 
 class beacon {
  private:
   std::string name;
-  vec_3d location;    // the real location of the beacon in the simulation (ground truth)
-  vec_3d estlocation; // the location the beacon believes it is in (with error, as in real life)
-  double localError;
-  double rangeError;
+  vec_3d location;  // the real location of the beacon in the simulation (ground
+                    // truth)
+  vec_3d estlocation;  // the location the beacon believes it is in (with error,
+                       // as in real life)
+                       // double localError;
+                       // double rangeError;
+  void estimate_loc();
 
  public:
-  beacon(std::string name, vec_3d location, double localError,
-         double rangeError);
+  beacon(std::string name, vec_3d location);
 
   friend std::ostream& operator<<(std::ostream& s, beacon a);
 };
